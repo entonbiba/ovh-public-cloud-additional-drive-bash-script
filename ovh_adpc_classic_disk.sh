be1e7c9968d52f33cdab58943cfe84f8d50e3285
Enton Biba <entonbiba@users.noreply.github.com>
2017-10-04 22:44:47 -0400

    Update ovh_adpc_classic_disk.sh

--- ovh_adpc_classic_disk.sh
+++ ovh_adpc_classic_disk.sh
@@ -6,8 +6,8 @@ sudo lsblk
 #
 # if its a high performance disk it will start with the letter v
 # vdb is the first additional drive and should be changed accordingly
-# other additional drives will be alpha increment on third character such as sdc sdd sde sdf...
-sudo fdisk /dev/sda <<EOF
+# other additional drives will be alpha increment on third character such as sdb sdc sdd sde sdf...
+sudo fdisk /dev/sdb <<EOF
 n
 
 
@@ -15,12 +15,12 @@ n
 w
 EOF
 #format the partition
-sudo mkfs.ext4 /dev/sda1
+sudo mkfs.ext4 /dev/sdb1
 <<EOF
 
 EOF
 # mount the additional drive
 sudo mkdir -p /mnt/disk
-sudo mount /dev/sda1 /mnt/disk/
+sudo mount /dev/sdb1 /mnt/disk/
 # view mounted drives
 sudo df -h
